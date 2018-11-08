19775de4084bc73e914384e4e85716524df664c0
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-11-08 12:42:04 +0100

    add import statement to Backup class

--- src/classes/Backup.py
+++ src/classes/Backup.py
@@ -1,8 +1,10 @@
+import pickle
+
 class Backup():
-	def save(self, object, name):
+	def save(self, object, databaseName):
 		with open('../../obj/' + name + '.pkl', 'wb') as f:
 			pickle.dump(object, f, pickle.HIGHEST_PROTOCOL)
 
-	def load(self, name):
+	def load(self, databaseName):
 		with open('../../obj/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
