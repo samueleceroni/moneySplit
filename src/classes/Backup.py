82bb5b1284cca3aa8967027606cdbff94f0e58d1
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-11-12 08:35:19 +0100

    add main to Backup in order to test the class and converted to static class

--- src/classes/Backup.py
+++ src/classes/Backup.py
@@ -1,24 +1,25 @@
 import pickle
 
 class Backup():
-	def save(self, object, databaseName):
-		with open('../../obj/' + name + '.pkl', 'wb') as f:
+	def save(object, databaseName):
+		with open('../../obj/' + databaseName + '.pkl', 'wb') as f:
 			pickle.dump(object, f, pickle.HIGHEST_PROTOCOL)
 
-	def load(self, databaseName):
-		with open('../../obj/' + name + '.pkl', 'rb') as f:
-        return pickle.load(f)
+	def load(databaseName):
+		with open('../../obj/' + databaseName + '.pkl', 'rb') as f:
+			return pickle.load(f)
 
+# TODO how to handle excpetion FileNotFoundException ? return null or try handle exceptions?
 
 def main():
 	a = 3
 	b = 4
 	Backup.save(a,"aVar")
-	Backup.save(a,"bVar")
+	Backup.save(b,"bVar")
 	a = Backup.load("aVar")
 	b = Backup.load("bVar")
-	print a+b
+	print (a+b)
 
 
 if __name__ == '__main__':
-    main()
+	main()
