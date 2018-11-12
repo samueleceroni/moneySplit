f9eebc3333e4fffe908cbe7516b78996c11a7cf9
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-11-12 08:26:51 +0100

    add main to Backup to test it

--- src/classes/Backup.py
+++ src/classes/Backup.py
@@ -8,3 +8,17 @@ class Backup():
 	def load(self, databaseName):
 		with open('../../obj/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
+
+
+def main():
+	a = 3
+	b = 4
+	Backup.save(a,"aVar")
+	Backup.save(a,"bVar")
+	a = Backup.load("aVar")
+	b = Backup.load("bVar")
+	print a+b
+
+
+if __name__ == '__main__':
+    main()
