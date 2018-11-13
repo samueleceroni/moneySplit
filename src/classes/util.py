094c003ff490024b5b1237431c6d7906caf3b382
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-11-13 12:48:11 +0100

    move backup content in util and test

--- src/classes/util.py
+++ src/classes/util.py
@@ -1,17 +1,40 @@
 import datetime
+import pickle
 
 class Util:
 
 	__logFileName = "log"
 
+	def __getActualMoment():
+		return datetime.datetime.now()
+
 	def log(logMessage):
-		now = datetime.datetime.now()
 		with open('../../res/' + Util.__logFileName + '.txt', 'a') as logFile:
-			logFile.write("[" + now.strftime("%Y-%m-%d %H:%M:%S" + "]: " +\
-						  "{" + logMessage + "}\n\n"))
+			logFile.write("[" + Util.getTimeDetailed() + "]: " +\
+						  "{" + logMessage + "}\n\n")
+	def getTime():
+		return Util.__getActualMoment().strftime("%Y-%m-%d %H:%M")
+
+	def getTimeDetailed():
+		return Util.__getActualMoment().strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Backup():
+    def save(object, databaseName):
+        with open('../../res/' + databaseName + '.pkl', 'wb') as databaseFile:
+            pickle.dump(object, databaseFile, pickle.HIGHEST_PROTOCOL)
+
+    def load(databaseName):
+        with open('../../res/' + databaseName + '.pkl', 'rb') as databaseFile:
+            return pickle.load(databaseFile)
+
 
 def main():
 	Util.log("Util.__logFileName is private and is not accessible")
+	print(Util.getTime())
+	print(Util.getTimeDetailed())
+	#print(Util.__getActualMoment())
+	
 
 if __name__ == '__main__':
 	main()
