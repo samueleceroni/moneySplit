47bd99aa9785693a83fb0692a1f0142180ff799b
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-11-15 00:07:08 +0100

    implement Parser, still to test

--- src/classes/Parser.py
+++ src/classes/Parser.py
@@ -49,45 +49,86 @@ class Parser:
 	def __parseAddList(self):
 		# "add listname"
 		self.__command = CommandsEnum.ADD_LIST
-		self.__listname = ''
-		for i in range(1, len(self.__wordsText) - 1)
-			self.__listname += self.__wordsText[i]
-		if self.__listname == '':
-			raise AttributeError("I can't add list if I haven't the listname, dude")
+		self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+		return
 
 	def __parseRemList(self):
 		self.__command = CommandsEnum.REM_LIST
-		self.__listname = ''
-		for i in range(1, len(self.__wordsText) - 1)
-			self.__listname += (self.__wordsText[i] + ' ')
-		if self.__listname == '':
-			raise AttributeError("I can't remove the list if I haven't the listname, dude")
+		self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+		return
+
 		
 	def __parseRemItem(self):
 		self.__command = CommandsEnum.REM_ITEM
+		if len(self.__wordsText) == 1:
+			raise AttributeError("You have to insert the name of the list and the index of the item")
 		try:
-			self.__itemNumToDelete = float(words[1])
+			checkDecimal = float(self.__wordsText[1])
+			checkDecimal = checkDecimal % 1
+			if checkDecimal != 0.0:
+				raise AttributeError("The number is not an integer, please insert a correct index")
+			self.__itemNumToDelete = int(self.__wordsText[1])
 			self.__supposeOneList = True
 			if len(self.__wordsText) > 2:
 				raise AttributeError("You entered too many parameters dude!")
 			return
-		except ValueError:
+		except ValueError: # the second word is not the number, so it is the name of the list
 			pass
-		
+		if len(self.__wordsText) <= 2:
+			raise AttributeError("You have to insert the name of the list and the index of the item")
+		# the last word should be the number
+		try:
+			checkDecimal = float(self.__wordsText[len(self.__wordsText) - 1])
+			checkDecimal = checkDecimal % 1
+			if checkDecimal != 0.0:
+				raise AttributeError("The number is not an integer, please insert a correct index")
+			self.__itemNumToDelete = int(self.__wordsText[len(self.__wordsText) - 1])
+			self.__supposeOneList = False
+		except AttributeError as exc:
+			raise exc
+		self.__listname = Parser.__parseNameList(self.__wordsText[1:-1])
+		return
 		
 	def __parseShow(self):
 		self.__command = CommandsEnum.SHOW_LIST
+		self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+		return
+
 		
 	def __parseTotal(self):
 		self.__command = CommandsEnum.SHOW_TOTAL
+		self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+		return
 		
-	def __parseAddItem(self):
-		self.__command = CommandsEnum.ADD_ITEM
-
 	def __parseShowListsNames(self):
 		self.__command = CommandsEnum.SHOW_ALL_LISTS_NAMES
-		
+		return
+
+	def __parseAddItem(self):
+		self.__command = CommandsEnum.ADD_ITEM
+		try:
+			checkNum = float(self.__wordsText[0])
+			self.__amount = checkNum
+			self.__supposeOneList = True
+			self.__description = Parser.__parseDescription(self.__wordsText[1:])
+			return
+		except ValueError:
+			pass
+		#check if at list an element is the price
+		for i in range(1, len(self.__wordsText) - 1):
+			try:
+				checkNum = float(self.__wordsText[i])
+				self.__amount = checkNum
+				self.__listname = self.__wordsText[:i]
+				self.__description = Parser.__parseDescription(self.__wordsText[i+1:])
+				return
+			except ValueError:
+				pass
+			except Exception:
+				raise Exception("Ops, something went wrong")
+		raise Excpetion("Ops, something went wrong")
 	
+#TODO check in form [:b] that b is not included
 
 	def getIdChat(self):
 		return self.__idChat
@@ -96,8 +137,19 @@ class Parser:
 		return self.__command
 
 
+	def __parseNameList(strVectListName):
+		listname = ''
+		for word in strVectListName
+			listname += word
+		if listname == '':
+			raise AttributeError("I can't remove the list if I haven't the listname, dude")
+		return listname
 
-
+	def __parseDescription(strVectDescription):
+		description = ''
+		for word in strVectDescription
+			description += word
+		return description
 
 
 
