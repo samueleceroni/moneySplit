c63f6b437bc7e5ca45f1e99e268b850135fde35b
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-11-15 13:20:12 +0100

    test ADD LIST and ADD ITEM parse

--- src/classes/Parser.py
+++ src/classes/Parser.py
@@ -9,10 +9,11 @@ class Parser:
     # keyToDelete
     # supposeOnlyOneList
 
-    def __init__(self, update):
-        self.__idChat = update['message']['chat']['id']
-        #self.__chatName = update['message']['chat'][..]
-        entireText = update['message']['text']
+#    def __init__(self, update):
+#        self.__idChat = update['message']['chat']['id']
+#        #self.__chatName = update['message']['chat'][..]
+#        entireText = update['message']['text']
+    def __init__ (self, entireText):
         # if there is, delete '/' character
         if entireText[0]=='/':
             entireText = entireText[1:]
@@ -49,12 +50,12 @@ class Parser:
     def __parseAddList(self):
         # "add listname"
         self.__command = CommandsEnum.ADD_LIST
-        self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+        self.__listname = Parser.__parseListName(self.__wordsText[1:])
         return
 
     def __parseRemList(self):
         self.__command = CommandsEnum.REM_LIST
-        self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+        self.__listname = Parser.__parseListName(self.__wordsText[1:])
         return
 
     def __parseRemItem(self):
@@ -85,17 +86,17 @@ class Parser:
             self.__supposeOneList = False
         except AttributeError as exc:
             raise exc
-        self.__listname = Parser.__parseNameList(self.__wordsText[1:-1])
+        self.__listname = Parser.__parseListName(self.__wordsText[1:-1])
         return
         
     def __parseShow(self):
         self.__command = CommandsEnum.SHOW_LIST
-        self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+        self.__listname = Parser.__parseListName(self.__wordsText[1:])
         return
 
     def __parseTotal(self):
         self.__command = CommandsEnum.SHOW_TOTAL
-        self.__listname = Parser.__parseNameList(self.__wordsText[1:])
+        self.__listname = Parser.__parseListName(self.__wordsText[1:])
         return
 
     def __parseShowListsNames(self):
@@ -112,51 +113,65 @@ class Parser:
             return
         except ValueError:
             pass
-        #check if at list an element is the price
+        #check if at least an element is the price
         for i in range(1, len(self.__wordsText) - 1):
             try:
                 checkNum = float(self.__wordsText[i])
                 self.__amount = checkNum
-                self.__listname = self.__wordsText[:i]
+                self.__listname = Parser.__parseListName(self.__wordsText[:i])
                 self.__description = Parser.__parseDescription(self.__wordsText[i+1:])
                 return
-            except ValueError:
+            except ValueError: # normal, when the current word is not a number
                 pass
             except Exception:
                 raise Exception("Ops, something went wrong")
-        raise Excpetion("Ops, something went wrong")
+        raise Exception("Dude you didn't tell me any amount!")
+    '''
+        STATIC PRIVATE FUNCTIONS
+    '''
+    def __lowerUntilNumber(strVectWords):
+        if type(strVectWords) == str:
+            Util.log(strVectWords + " is a string, not an array of String")
+            raise AttributeError("This is a string, not an array of String")
+        for i in range(len(strVectWords)):
+            try:
+                float(strVectWords[i])
+                break
+            except ValueError:
+                strVectWords[i] = strVectWords[i].lower()
+        return strVectWords
 
+    def __parseListName(strVectListName):
+        listname = Parser.__linkWordsFromVector(strVectListName)
+        if listname == '':
+            raise AttributeError("I can't remove the list if I haven't the listname, dude")
+        return listname
+
+    def __parseDescription(strVectDescription):
+        return Parser.__linkWordsFromVector(strVectDescription)
+
+    def __linkWordsFromVector(strVect):
+        linkedWords = ''
+        for word in strVect:
+            linkedWords += word
+        return linkedWords
+    '''
+        GETTERS
+    '''
     def getIdChat(self):
         return self.__idChat
 
     def getCommand(self):
         return self.__command
 
-    def __lowerUntilNumber(strVectWords):
-        if type(strVectWords) == str:
-            Util.log(strVectWords + " is a string, not an array of String")
-            raise AttributeError("This is a string, not an array of String")
-        for word in strVectWords:
-            try:
-                float(word)
-                break
-            except ValueError:
-                word = word.lower()
-        return strVectWords
+    def getListName(self):
+        return self.__listname
 
-    def __parseNameList(strVectListName):
-        listname = ''
-        for word in strVectListName:
-            listname += word
-        if listname == '':
-            raise AttributeError("I can't remove the list if I haven't the listname, dude")
-        return listname
+    def getAmount(self):
+        return self.__amount
 
-    def __parseDescription(strVectDescription):
-        description = ''
-        for word in strVectDescription:
-            description += word
-        return description
+    def getDescription(self):
+        return self.__description
 
 """
 AVAIABLE COMMAND:
@@ -174,12 +189,33 @@ SHOW TOTAL LIST -> total 'listname'
 
 """
 
-'''
-def main():
-    # test goes here
 
+def main():
+    queryString = "add sbornList"
+    parsedQuery = Parser(queryString)
+    print(parsedQuery.getCommand() == CommandsEnum.ADD_LIST)
+    print(parsedQuery.getListName() == "sbornlist")
+    queryString = "Add sbornList"
+    parsedQuery = Parser(queryString)
+    print(parsedQuery.getCommand() == CommandsEnum.ADD_LIST)
+    print((parsedQuery.getListName() == "sbornList") == False)
+    print("First test Completed : ADD_LIST\n")
+
+    queryString = "sbornList 3 sbornDescription"
+    parsedQuery = Parser(queryString)
+    print(parsedQuery.getCommand() == CommandsEnum.ADD_ITEM)
+    print(parsedQuery.getListName() == "sbornlist")
+    print(parsedQuery.getAmount() == 3.0)
+    print(parsedQuery.getDescription() == "sbornDescription")
+    print((parsedQuery.getDescription() == "sborndescription") == False)
+    print("Second test Completed : ADD_ITEM\n")
+
+''' queryString = "Add sbornList"
+    parsedQuery = Parser(queryString)
+    print(parsedQuery.getCommand() == CommandsEnum.ADD_LIST)
+    print((parsedQuery.getListName() == "sbornList") == False)
+'''
 
 
 if __name__ == '__main__':
     main()
-'''
